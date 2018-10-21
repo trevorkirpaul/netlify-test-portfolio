b78bd257085d50fd67e7ad9303acffcc650da095
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-10-21 12:06:15 -0400

    removed boilerplate, updated styles and app contents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="WrapperBase">
+        <h1>Trevor Kirpaul</h1>
+
+        <div class="BlockWrap"><h3>Description</h3>
+        
+        <p>Front-end Web Developer</p></div>
+
+        <div class="BlockWrap">
+          <h3>Favorites</h3>
+          <ul>
+          <li>
+          React
+          </li>
+          <li>
+          GraphQL
+          </li>
+          <li>
+          Node.js
+          </li>
+          </ul>
+        </div>
+
+        <div class="BlockWrap"><h3>Links</h3>
+        <a href="https://github.com/trevorkirpaul">Github</a></div>
       </div>
     );
   }
