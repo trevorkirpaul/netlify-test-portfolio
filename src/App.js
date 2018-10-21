82649b9dd209cc8e39534fc45efb99a80a313c55
Trevor Kirpaul <trevorkirpaul@gmail.com>
2018-10-21 12:42:30 -0400

    updated styling. added font awesome icons and mobile styling

--- src/App.js
+++ src/App.js
@@ -5,30 +5,34 @@ class App extends Component {
 
   render() {
     return (
-      <div className="WrapperBase">
-        <h1>Trevor Kirpaul</h1>
-
-        <div class="BlockWrap"><h3>Description</h3>
-        
-        <p>Front-end Web Developer</p></div>
-
-        <div class="BlockWrap">
-          <h3>Favorites</h3>
-          <ul>
-          <li>
-          React
-          </li>
-          <li>
-          GraphQL
-          </li>
-          <li>
-          Node.js
-          </li>
-          </ul>
+      <div className="AppWrapper">
+        <div className="TitlePanel">
+          <h1>Trevor Kirpaul <span className="lightText desktop">Web Developer</span></h1>
+          <h2 className="mobileOnly">Web Developer</h2>
         </div>
 
-        <div class="BlockWrap"><h3>Links</h3>
-        <a href="https://github.com/trevorkirpaul">Github</a></div>
+        <div className="descriptionPanel">
+          <p>I love creating.</p>
+          <p>I'm currently focused on React, GraphQL and Node.js</p>
+        </div>
+
+        <div className="linksPanel">
+        <a
+          href="https://github.com/trevorkirpaul"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <i className="fab fa-github-square"></i> Personal GitHub
+        </a>
+
+        <a
+          href="https://twitter.com/trevorkirpaul"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <i className="fab fa-twitter-square"></i> Contact Me
+        </a>
+        </div>
       </div>
     );
   }
