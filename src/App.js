f76b515d17fb271e1c1aa0a410fe2fd4a0063502
Trevor Kirpaul <trevorkirpaul@gmail.com>
2019-01-10 21:40:15 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import configureStore from '@redux/store';
+import Router from './router';
 
 const store = configureStore();
 
@@ -10,9 +11,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="AppWrapper">
-          <h1>trevorKIRPAUL</h1>
-        </div>
+        <Router />
       </Provider>
     );
   }
---
+++ src/containers/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>about</li>
+
+            <li>posts</li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
